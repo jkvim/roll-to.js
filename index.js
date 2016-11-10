7bc1c387a926fa1f3af68b8ffd8b8f1a03dfbca8
jkvim <jkvim@outlook.com>
2016-11-10 13:44:03 +0800

    add api

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-module.exports = {
-  
-};
+module.exports = function() {
+  function top(option = {}) {
+
+  }
+
+  function bottom(option = {}) {
+    
+  }
+
+  function el(element, option = {}) {
+
+  }
+
+  return {
+    el,
+    top,
+    bottom,
+  };
+}();
