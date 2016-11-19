3e19f23af5d2639ee281f9ff1360541ffa490b92
jkvim <jkvim@outlook.com>
2016-11-19 17:06:35 +0800

    Update 2016-11-19T09:06:27.931Z

--- index.js
+++ index.js
@@ -1,30 +1,94 @@
-/*
- * roll-to
- * scroll library
- * @author jkvim
- * @license MIT
- * @version 0.0.1
- */
-
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
   (global.RollTo = factory());
 }(this, (function () { 'use strict';
 
-const raf = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAniamtionFrame || function (callback) {
-  setTimeout(callback, 1000 / 60);
-};
+const raf = window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.oRequestAnimationFrame ||
+  window.msRequestAniamtionFrame ||
+  function (callback) {
+    setTimeout(callback, 1000 / 60);
+  };
 
+// copy from https://gist.github.com/gre/1650294
 const easeFunctions = {
-  easeOut(t, d) {
+  easeOut(t) {
     return -1 * t * (t - 2);
+  },
+  // no easing, no acceleration
+  linear(t) {
+    return t
+  },
+  // accelerating from zero velocity
+  easeInQuad(t) {
+    return t * t
+  },
+  // decelerating to zero velocity
+  easeOutQuad(t) {
+    return t * (2 - t)
+  },
+  // acceleration until halfway, then deceleration
+  easeInOutQuad(t) {
+    return t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t
+  },
+  // accelerating from zero velocity 
+  easeInCubic(t) {
+    return t * t * t
+  },
+  // decelerating to zero velocity 
+  easeOutCubic(t) {
+    return (--t) * t * t + 1
+  },
+  // acceleration until halfway, then deceleration 
+  easeInOutCubic(t) {
+    return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
+  },
+  // accelerating from zero velocity 
+  easeInQuart(t) {
+    return t * t * t * t
+  },
+  // decelerating to zero velocity 
+  easeOutQuart(t) {
+    return 1 - (--t) * t * t * t
+  },
+  // acceleration until halfway, then deceleration
+  easeInOutQuart(t) {
+    return t < .5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t
+  },
+  // accelerating from zero velocity
+  easeInQuint(t) {
+    return t * t * t * t * t
+  },
+  // decelerating to zero velocity
+  easeOutQuint(t) {
+    return 1 + (--t) * t * t * t * t
+  },
+  // acceleration until halfway, then deceleration 
+  easeInOutQuint(t) {
+    return t < .5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t
   }
 };
 
 const defaultOption = {
   animate: 'easeOut',
-  duration: 0.5
+  duration: 1,
+};
+
+const checkOption = (option) => {
+  for (let key in defaultOption) {
+    if (!(key in option)) {
+      option[key] = defaultOption[key];
+    }
+  }
+  if (!easeFunctions[option.animate]) {
+    option.animate = defaultOption.animate;
+  }
+  if (option.duration <= 0) {
+    option.duration = defaultOption.duration;
+  }
 };
 
 const RollTo = function (option = defaultOption) {
@@ -32,25 +96,29 @@ const RollTo = function (option = defaultOption) {
   let initPosition = null;
   let wrapper = null;
 
+  checkOption(option);
+
   const reset = () => {
     startime = null;
     initPosition = null;
   };
 
-  const getWrapperHeight = wrapper => {
-    return wrapper === document.body ? wrapper.clientHeight : wrapper.scrollHeight;
-    
+  const getWrapperHeight = (wrapper) => {
+    return wrapper.scrollHeight;
   };
 
-  const getViewHeight = wrapper => {
-    return wrapper === document.body ? window.innerHeight : wrapper.clientHeight;
+  const getViewHeight = (wrapper) => {
+    return wrapper === document.body ?
+      window.innerHeight :
+      wrapper.clientHeight;
   };
 
-  function getNodeOffsetTop(el) {
+
+  const getNodeOffsetTop = (el) => {
     var y = el.offsetTop;
     var node = el.offsetParent;
     return y + node.offsetTop;
-  }
+  };
 
   const doScroll = (option, timestamp, offsetY) => {
     if (!initPosition) {
@@ -69,8 +137,8 @@ const RollTo = function (option = defaultOption) {
     wrapper.scrollTop = initPosition + distance * v;
   };
 
-  const scrollMixin = calcPosition => {
-    return element => {
+  const scrollMixin = (calcPosition) => {
+    return (element) => {
       function scroll(element, timestamp) {
         if (!element) {
           throw Error('required element as argument');
@@ -89,7 +157,7 @@ const RollTo = function (option = defaultOption) {
         raf(scroll.bind(null, element));
       }
       raf(scroll.bind(null, element));
-    };
+    }
   };
 
   const top = scrollMixin((element, timestamp) => {
@@ -98,7 +166,7 @@ const RollTo = function (option = defaultOption) {
     let atTop = offsetY === scrollY;
     return {
       stop: atTop,
-      offsetY
+      offsetY,
     };
   });
 
@@ -112,7 +180,7 @@ const RollTo = function (option = defaultOption) {
 
     return {
       stop: atBottom,
-      offsetY
+      offsetY,
     };
   });
 
@@ -126,20 +194,21 @@ const RollTo = function (option = defaultOption) {
     let viewHeight = getViewHeight(wrapper);
 
     // when element reach top, offsetY must equal to scrollY
+
     let atBottom = wrapperHeight - viewHeight === scrollY;
     let atTop = offsetY === scrollY;
-    let isLast = offsetY + height === wrapperHeight;
+    let insideView = offsetY > scrollY && (offsetY - scrollY) < viewHeight;
 
     return {
       offsetY,
-      stop: atTop || isLast && atBottom
+      stop: atTop || (insideView && atBottom)
     };
   });
 
   return {
     top,
     bottom,
-    section
+    section,
   };
 };
 
